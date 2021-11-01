f08056795fcb753ff3ad154a9fee8461a08000f1
Himanshu <helloitshimzz@gmail.com>
2021-11-01 07:58:43 +0530

    first commit

---
+++ src/components/SingleCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./SingleCard.css";
+
+function SingleCard({ card, handleChoice }) {
+  const handleClick = () => {
+    handleChoice(card);
+  };
+  return (
+    <div className="card">
+      <div>
+        <img className="front" src={card.src} alt="card front" />
+        <img
+          onClick={handleClick}
+          className="back"
+          src="/img/cover.png"
+          alt="card back"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default SingleCard;
